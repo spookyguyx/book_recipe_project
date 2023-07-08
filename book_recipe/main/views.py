643317413f90ef0c307cd5e2c0432fdecb2c9fa5
Victorya <victoryaaam@mail.ru>
2023-07-08 23:27:59 +0300

    Добавила страницу регистрации и входа

--- book_recipe/main/views.py
+++ book_recipe/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return render(request, 'main/breakfast.html')
+    return render(request, 'main/index.html')
 
 
 def about(request):
