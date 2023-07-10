c989dfef1da0d077f3cb31ef6ab531449c933ab8
Badmac <mr.ds53@mail.ru>
2023-07-10 19:05:01 +0300

    added URLS to breakfast, lunch, main button,

--- book_recipe/main/views.py
+++ book_recipe/main/views.py
@@ -14,5 +14,33 @@ def breackfast(request):
     return render(request, 'main/breakfast.html')
 
 
-def lanch(request):
-    return render(request, 'main/lanch.html')
+def lunch(request):
+    return render(request, 'main/lunch.html')
+
+
+def recipes_launch(request):
+    return render(request, 'main/recipes_launch.html')
+
+
+def recept1(request):
+    return render(request, 'main/Recept1.html')
+
+
+def registration(request):
+    return render(request, 'main/registration.html')
+
+
+def signin(request):
+    return render(request, 'main/signIn.html')
+
+
+#def dinner(request):
+#    return render(request, 'main/dinner.html')
+
+
+#def desert(request):
+#    return render(request, 'main/desert.html')
+
+
+#def favourite(request):
+#    return render(request, 'main/favourite.html')
