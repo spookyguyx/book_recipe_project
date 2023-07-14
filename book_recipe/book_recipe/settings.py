60c8a0d7a3adade6792ebcf81781234e143b06db
Trmtrg <Vitalik.petrackov@yandex.ru>
2023-07-14 04:53:09 +0300

    Выровнял статик в настройках

--- book_recipe/book_recipe/settings.py
+++ book_recipe/book_recipe/settings.py
@@ -123,14 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    'main/static/',
-    'users/static/',
+    os.path.join(BASE_DIR, "main", "static"),
+    os.path.join(BASE_DIR, "users", "static"),
 ]
 
 # Default primary key field type
