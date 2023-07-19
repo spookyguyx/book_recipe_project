550e1aeb8106f6b184c83f73120b1e0f4fe8968f
Badmac <mr.ds53@mail.ru>
2023-07-19 13:27:51 +0300

    fixed auth add recipe button

--- book_recipe/main/urls.py
+++ book_recipe/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import toggle_favorite
 
 urlpatterns = [
     path('', views.index, name='home'),
@@ -20,6 +21,10 @@ urlpatterns = [
     path('dinner/<int:pk>', views.RecipeId.as_view(), name='recipe_detail2'),
     path('dessert/<int:pk>', views.RecipeId.as_view(), name='recipe_detail3'),
     path('drink/<int:pk>', views.RecipeId.as_view(), name='recipe_detail4'),
+    path('recipe/<int:recipe_id>/toggle_favorite/', toggle_favorite, name='toggle_favorite'),
+    #path('recipe/<int:recipe_id>/add_to_favorites/', views.add_to_favorites, name='add_to_favorites'),
+    #path('recipe/<int:recipe_id>/remove_from_favorites/', views.remove_from_favorites, name='remove_from_favorites'),
+    path('profile/', views.profile, name='profile'),
 
 ]
 
--- book_recipe/main/views.py
+++ book_recipe/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .forms import RecipeForm
 from .models import Recipe
@@ -100,6 +100,38 @@ def drink(request):
 
 def favorites(request):
     return render(request, 'main/favorites.html')
+
+'''
+def add_to_favorites(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    recipe.is_favorite = True
+    recipe.save()
+    return redirect('recipe_detail', recipe_id=recipe_id)
+
+
+def remove_from_favorites(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    recipe.is_favorite = False
+    recipe.save()
+    return redirect('recipe_detail', recipe_id=recipe_id)#}
+'''
+
+
+def toggle_favorite(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    user = request.user
+
+    if user in recipe.users_favorite.all():
+        recipe.users_favorite.remove(user)  # Удалить статью из избранного
+    else:
+        recipe.users_favorite.add(user)  # Добавить статью в избранное
+
+    return redirect('recipe_detail', pk=recipe_id)
+
+
+def profile(request):
+    user = request.user
+    return render(request, 'profile.html', {'user': user})
 
 
 class RecipeId(DetailView):
