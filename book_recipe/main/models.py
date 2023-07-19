2d939e8b9b33ea0ed7c0a3ffb30c60a998b07938
Snakeb1te5 <kopkov.d@gmail.com>
2023-07-19 15:50:21 +0300

    Перед миграцией

--- book_recipe/main/models.py
+++ book_recipe/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 CATEGORY_CHOICES = (
     ('breakfast', 'BREAKFAST'),
@@ -10,6 +11,7 @@ CATEGORY_CHOICES = (
 
 
 class Recipe(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField('Название блюда', max_length=50)
     image = models.ImageField(upload_to='image/')
     ingredients = models.CharField('Ингредиенты', max_length=300)
@@ -19,9 +21,12 @@ class Recipe(models.Model):
     comments = models.TextField('Комментарии')
     rating = models.IntegerField(default=0)
     category = models.CharField(max_length=9, choices=CATEGORY_CHOICES, default='breakfast')
+    online = models.BooleanField(default= False)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return f'{self.id}'
+
+
